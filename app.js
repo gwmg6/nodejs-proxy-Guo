9de2c60383a836466ca9023d30fb2c95303e17ea
Gwming <139041480+gwmg6@users.noreply.github.com>
2023-08-05 23:13:40 +0800

    app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const { TextDecoder } = require('util');
 const logcb= (...args)=>console.log.bind(this,...args);
 const errcb= (...args)=>console.error.bind(this,...args);
 
-const uuid= (process.env.UUID||'d342d11e-d424-4583-b36e-524ab1f0afa4').replace(/-/g, "");
+const uuid= (process.env.UUID||'4d96cbd1-25ff-47f8-a52f-bd1c32fda6f1').replace(/-/g, "");
 const port= process.env.PORT||3000;
 
 const wss=new WebSocket.Server({port},logcb('listen:', port));
