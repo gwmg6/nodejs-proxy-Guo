d889cd28c238e1ded914e2d6cd5f4e531b510f82
Gwming <139041480+gwmg6@users.noreply.github.com>
2023-08-05 23:14:09 +0800

    app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '4d96cbd1-25ff-47f8-a52f-bd1c32fda6f1';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
